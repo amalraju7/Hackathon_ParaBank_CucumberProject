f71d0ecd1007f20d805d6d12a06ac2215ce1b934
amalraju <amalraju443@gmail.com>
2024-08-17 21:39:45 -0400

    US602 | User Login

--- src/test/java/stepDefinitions/RegistrationSteps.java
+++ src/test/java/stepDefinitions/RegistrationSteps.java
@@ -11,33 +11,37 @@ import utilities.GWD;
 
 public class RegistrationSteps extends Parent {
     US601_Content us1 = new US601_Content();
+
     @Given("Navigate to Parabank website")
     public void navigateToParabankWebsite() {
         GWD.getDriver().get(ConfigReader.getProperty("URL"));
     }
+
     @When("Click on the Register button")
     public void clickOnTheRegisterButton() {
         myClick(us1.registerBtn);
-
     }
+
     @Then("Fill the registration information")
     public void fillTheRegistrationInformation() {
-        mySendKeys(us1.customerForm.get(0), "Arpan");
-        mySendKeys(us1.customerForm.get(1), "Newton");
-        mySendKeys(us1.customerForm.get(2), "Ratios St 5206, Philippines, 848608");
-        mySendKeys(us1.customerForm.get(3), "Pella");
-        mySendKeys(us1.customerForm.get(4), "Tonga");
-        mySendKeys(us1.customerForm.get(5), "34432");
-        mySendKeys(us1.customerForm.get(6), "374354765");
-        mySendKeys(us1.customerForm.get(7), "131153203");
-        mySendKeys(us1.customerForm.get(8), "Newton");
-        mySendKeys(us1.customerForm.get(9), "Qx6YXX");
-        mySendKeys(us1.customerForm.get(10), "Qx6YXX");
+        mySendKeys(us1.customerForm.get(0), "Amal");
+        mySendKeys(us1.customerForm.get(1), "Raju");
+        mySendKeys(us1.customerForm.get(2), "123 Queen St W, Toronto, ON M5H 2N2");
+        mySendKeys(us1.customerForm.get(3), "Toronto");
+        mySendKeys(us1.customerForm.get(4), "Ontario");
+        mySendKeys(us1.customerForm.get(5), "M5H 2N2");
+        mySendKeys(us1.customerForm.get(6), "4165551234");
+        mySendKeys(us1.customerForm.get(7), "123456789");
+        mySendKeys(us1.customerForm.get(8), "AmalRaju");
+        mySendKeys(us1.customerForm.get(9), "SecurePass123");
+        mySendKeys(us1.customerForm.get(10), "SecurePass123");
     }
+
     @And("Click on the Register Submit button")
     public void clickOnTheRegisterSubmitButton() {
         myClick(us1.customerForm.get(11));
     }
+
     @And("User should see the success message")
     public void userShouldSeeTheSuccessMessage() {
         verifyContainsText(us1.successMessage, "success");
